c8a66ac6b3a042ca76ffa5cfc7fd9ded3e649cf1
Temitope Samson Isaiah <61621188+smit1298@users.noreply.github.com>
2022-04-20 23:45:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" }
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  const result = Joi.Validate(req.body, schema);
+
+  if (result.error) {
+    res
+      .status(400).send(result.error);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found");
+  res.send(course);
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}`));
